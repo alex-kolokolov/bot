c98f43e4d3439446c0690c36d99d5022b6b073f0
mario <mariokurwaq337@gmail.com>
2021-09-12 23:37:11 +0400

    first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters, errors
 from time import sleep
-
+import re
 
 app = Client("my_account")
 
@@ -10,6 +10,50 @@ def dynamic_data_filter(data):
         lambda flt, _, query: flt.data == query.data,
         data=data  # "data" kwarg is accessed with "flt.data" above
     )
+
+
+@app.on_message(filters.command("type", prefixes='.') & filters.me)
+def type(_, msg):
+    print(msg)
+    orig_text = msg.text.split(".type ", maxsplit=1)[1]
+    text = orig_text
+    tbp = ''
+    chat_id, message_id = msg['chat']['id'], msg['message_id']
+    typing_symbol = '▒'
+    while(tbp != orig_text):
+        try:
+            app.edit_message_text(chat_id, message_id, f'<b>{tbp + typing_symbol}</b>', parse_mode='html')
+            sleep(0.15)
+
+            tbp = tbp + str(text)[0]
+            text = text[1:]
+            print(tbp)
+
+            app.edit_message_text(chat_id, message_id, f'<b>{tbp}</b>', parse_mode='html')
+            sleep(0.15)
+        except errors.FloodWait as e:
+            sleep(e.x)
+
+
+@app.on_message(filters.command("b", prefixes='.') & filters.me)
+def bold(_, msg):
+    orig_text = re.split(".B |.b ", msg.text, maxsplit=1)[1]
+    chat_id, message_id = msg['chat']['id'], msg['message_id']
+    app.edit_message_text(chat_id, message_id, f'<b>{orig_text}</b>', parse_mode='html')
+
+
+@app.on_message(filters.command("i", prefixes='.') & filters.me)
+def italic(_, msg):
+    orig_text = re.split(".I |.i ", msg.text, maxsplit=1)[1]
+    chat_id, message_id = msg['chat']['id'], msg['message_id']
+    app.edit_message_text(chat_id, message_id, f'<i>{orig_text}</i>', parse_mode='html')
+
+
+@app.on_message(filters.command("bi", prefixes='.') & filters.me)
+def bold_and_italic(_, msg):
+    orig_text = re.split(".Bi |.bi ", msg.text, maxsplit=1)[1]
+    chat_id, message_id = msg['chat']['id'], msg['message_id']
+    app.edit_message_text(chat_id, message_id, f'<b><i>{orig_text}</i></b>', parse_mode='html')
 
 
 @app.on_message(filters.command("wave", prefixes='.') & filters.me)
